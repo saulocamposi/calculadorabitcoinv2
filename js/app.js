ee1e582e93a4a1d5a4bf714d3f984befeea55f53
saulocamposi <saulocamposi@gmail.com>
2016-04-07 23:42:31 -0300

    Add app.js for mdoules

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+require("angular");
+require("angular-route");
+require("./manager-module.js");
+require("./calc-routes.js");
+require("./value.js");
+
+
+require("../css/bootstrap-custom.css");
+require("../css/calc_bitcoin.css");
+require("../css/calc_income.css");
+require("../css/card.css");
+require("../css/main.css");
+require("../css/main_calculator.css");
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+
+  entry: [
+    'bootstrap-loader',
+    './js/app.js',
+  ],
+  output: {
+    path: path.join(__dirname, 'public', 'assets'),
+      filename: 'app.js',
+      publicPath: '/public/assets/',
+  },
+  module: {
+      loaders: [
+        { test: /\.css$/, loaders: [ 'style', 'css', 'postcss' ] },
+        { test: /\.scss$/, loaders: [ 'style', 'css', 'postcss', 'sass' ] },
+        { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports?jQuery=jquery' },
+  //      {
+          // test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          // loader: "url?limit=10000"
+        // },
+        // {
+          // test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+          // loader: 'file'
+        // },
+        {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+         loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
+}
+
+        // Bootstrap 3
+
+      ],
+    },
+
+    postcss: [ autoprefixer ],
+
+
+};
