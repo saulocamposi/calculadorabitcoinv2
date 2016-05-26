25db95591d359b5d11dc14d61c76cdfe6c1cc7a2
saulocamposi <saulocamposi@gmail.com>
2016-05-25 22:30:04 -0300

    Added devtool param

--- webpack.config.js
+++ webpack.config.js
@@ -10,33 +10,38 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, 'public', 'assets'),
-      filename: 'app.js',
-      publicPath: '/public/assets/',
+    filename: 'app.js',
+    publicPath: '/public/assets/',
   },
   module: {
-      loaders: [
-        { test: /\.css$/, loaders: [ 'style', 'css', 'postcss' ] },
-        { test: /\.scss$/, loaders: [ 'style', 'css', 'postcss', 'sass' ] },
-        { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports?jQuery=jquery' },
-  //      {
-          // test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          // loader: "url?limit=10000"
-        // },
-        // {
-          // test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-          // loader: 'file'
-        // },
-        {
+    loaders: [{
+        test: /\.css$/,
+        loaders: ['style', 'css', 'postcss']
+      }, {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'postcss', 'sass']
+      }, {
+        test: /bootstrap-sass\/assets\/javascripts\//,
+        loader: 'imports?jQuery=jquery'
+      },
+      //      {
+      // test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      // loader: "url?limit=10000"
+      // },
+      // {
+      // test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+      // loader: 'file'
+      // },
+      {
         test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-         loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
-}
+        loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
+      }
 
-        // Bootstrap 3
+      // Bootstrap 3
 
-      ],
-    },
+    ],
+  },
 
-    postcss: [ autoprefixer ],
-
-
+  postcss: [autoprefixer],
+  devtool: 'sourcemap',
 };
