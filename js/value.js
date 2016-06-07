45ff7bd65da31772940f73e72480b1bc2a69e9f4
saulocamposi <saulocamposi@gmail.com>
2016-06-07 18:52:09 -0300

    Fixed js value

--- js/value.js
+++ js/value.js
@@ -6,69 +6,16 @@ $scope.helloforex = serviceforex;
  console.log("First Log" + serviceforex);
  console.log("Second Log" + $scope.helloforex);
 
-var restServices = function() {
-  $http.get("services/services.php?brand=coinbr").then(function(response) {
-    $scope.coinbr = response.data;
-  });
 
-  /**************************************************/
-  /*          Main Card                              */
-  /*************************************************/
-
-
-  $http.get("services/services.php?brand=mtc").then(function(response) {
-    $scope.mtc = response.data;
-  });
-
-  $http.get("services/services.php?brand=btctoyou").then(function(response) {
-    $scope.btctoyou = response.data;
-  });
-
-  $http.get("services/services.php?brand=flowbtc").then(function(response) {
-    $scope.flowbtc = response.data;
-  });
-
-  $http.get("services/services.php?brand=negociecoins").then(function(response) {
-    $scope.negociecoins = response.data;
-  });
-  };
-
-restServices();
+ $http.get("services/services.php?brand=coinbr").then(function(response) {
+   $scope.coinbr = response.data;
+ });
 
   $http.get("services/youtube-search.php").then(function(response) {
     $scope.dailydecrypt = response.data;
   });
 
-  /****************************************/
-  /*             FOX BIT SERVICES         */
-  /****************************************/
-  $http.jsonp("https://api.blinktrade.com/api/v1/BRL/ticker?crypto_currency=BTC&callback=JSON_CALLBACK").then(function(response) {
-    $scope.foxbit = response.data;
-  });
 
-  $http.get("services/orders_foxbit_services.php").then(function(response) {
-    $scope.foxbitorders = response.data;
-  });
-
-  $http.jsonp("https://api.blinktrade.com/api/v1/BRL/trades?crypto_currency=BTC&callback=JSON_CALLBACK").then(function(response) {
-    $scope.foxbittrades = response.data;
-  });
-
-  /****************************************/
-  /*             BLINKTRADE SERVICES         */
-  /****************************************/
-
-  $http.jsonp("https://api.blinktrade.com/api/v1/VEF/ticker?crypto_currency=BTC&callback=JSON_CALLBACK").then(function(response) {
-    $scope.surbitcoin = response.data;
-  });
-
-  $http.jsonp("https://api.blinktrade.com/api/v1/CLP/ticker?crypto_currency=BTC&callback=JSON_CALLBACK").then(function(response) {
-    $scope.chilebit = response.data;
-  });
-
-  $http.jsonp("https://api.blinktrade.com/api/v1/PKR/ticker?crypto_currency=BTC&callback=JSON_CALLBACK").then(function(response) {
-    $scope.urdubit = response.data;
-  });
 
   /********************************************/
 
@@ -82,23 +29,7 @@ restServices();
 
   $http.get("services/forex_services.php").then(function(response) {
     $scope.forex = response.data;
-  });
-
-  $http.get("services/doge_polionex_services.php").then(function(response) {
-    $scope.doge = response.data;
-  });
-
-  $http.get("services/dash_polionex_services.php").then(function(response) {
-    $scope.dash = response.data;
-  });
-
-  $http.get("services/lite_polionex_services.php").then(function(response) {
-    $scope.lite = response.data;
-  });
-
-  $http.get("services/ether_polionex_services.php").then(function(response) {
-    $scope.ether = response.data;
-  });
+  });  
 
   $http.get("services/walletfoxbit_cold_services.php").then(function(response) {
     $scope.wfoxcold = response.data;
