261fbceea9200a41e3fcb2aec46304ae97e55c5a
saulocamposi <saulocamposi@gmail.com>
2016-06-12 10:14:15 -0300

    Add blinktrade chart service mock data value

--- js/services/exchangesChartService.js
+++ js/services/exchangesChartService.js
@@ -1,92 +1,40 @@
 var app = require('../manager-module.js');
 
 var exchangesChartService = function() {
-  /*var chart = {
-                "type": "LineChart",
-                "options": {
-                    "title": "Lines to horizon",
-                    "pointSize": 7,
-                    "width" : 200,
-                    "height":200,
-                    "chartArea": {left:80,top:80,width:"70%",height:"70%"},
-                    "vAxis": {
-                        "title": "Vertical Data",
-                        "gridlines": {
-                            "count": 10
-                        }
-                    },
-                    "hAxis": {
-                        "title": "Horizontal Data"
-                    }
-                },
-                "data": {
-                    "cols": [
-                        {
-                            "id": "head",
-                            "label": "Label for HEAD",
-                            "type": "number",
-                            "p": {}
-                        },
-                        {
-                            "id": "line1",
-                            "label": "Line 1",
-                            "type": "number",
-                            "p": {}
-                        },
-                        {
-                            "id": "line2",
-                            "label": "Line 2",
-                            "type": "number",
-                            "p": {}
-                        },
-                        {
-                            "id": "line3",
-                            "label": "Line 3",
-                            "type": "number",
-                            "p": {}
-                        },
-                        {
-                            "id": "line4",
-                            "label": "Line 4",
-                            "type": "number",
-                            "p": {}
-                        },
-                        {
-                            "id": "line5",
-                            "label": "Line 5",
-                            "type": "number",
-                            "p": {}
-                        },
-                    ]}
-            };
-            */
-
   var chart = {
-    "type": "Bar",
+    "type": "BarChart",
     "options": {
-      "title": "Lines to horizon",
-      "pointSize": 7,
-      "width": 200,
-      "height": 200,
+      "title": "Exchanges Brazil",
+      "pointSize": 100,
+      "width": 600,
+      "height": 250,
       "chartArea": {
-        left: 80,
-        top: 80,
+        left: 90,
+        top: 40,
         width: "70%",
         height: "70%"
       }
     },
-    "data":  {
-      "cols" : [
-        {"id":"1","label":"Mercado","pattern":"","type":"number"},
-        {"id":"2","label":"Foxbit","pattern":"","type":"number"},
-        {"id":"3","label":"btctoyou","pattern":"","type":"number"},
-        {"id":"4","label":"negociecoins","pattern":"","type":"number"}
-      ],
-      "rows" : [10,10,10,10]
+    "data": {
+      "cols": [{
+        "id": "",
+        "label": "exchange",
+        "pattern":"",
+        "type": "string"
+      }, {
+        "id": "",
+        "label": "Preço",
+        "pattern":"",
+        "type": "number"
+      }],
+      "rows" : [
+        {"c" : [{"v" : "Mercado", "f": null},{"v" : 2236, "f": null}]},
+        {"c" : [{"v" : "Negocie Coins", "f": null},{"v" : 2200, "f": null}]},
+        {"c" : [{"v" : "Bitcointoyou", "f": null},{"v" : 2210, "f": null}]},
+        {"c" : [{"v" : "Fox", "f": null},{"v" : 2245, "f": null}]}
+      ]
     }
-}
-
-
+  };
 return chart;
 }
 
