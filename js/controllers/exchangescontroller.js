d6570859c32d318051f1bee4e0b3b591a0f04a63
saulocamposi <saulocamposi@gmail.com>
2016-06-12 14:29:44 -0300

    Fix data rows chart value

--- js/controllers/exchangescontroller.js
+++ js/controllers/exchangescontroller.js
@@ -2,26 +2,40 @@ var app = require('../manager-module.js');
 
 app.controller('exchangescontroller', ['$scope','$http', 'exchangesChartService', function($scope, $http, exchangesChartService) {
 
-    $scope.chart = exchangesChartService;
+  var dataChart = [];
 
     $http.jsonp("https://api.blinktrade.com/api/v1/BRL/ticker?crypto_currency=BTC&callback=JSON_CALLBACK").then(function(response) {
-      $scope.foxbit = response.data;
+      var datafoxbit = response.data;
+      dataChart.push({"c" : [{"v" : "FoxBit", "f": null},{"v" : datafoxbit.last, "f": null}]});
+      $scope.foxbit = datafoxbit;
     });
 
     $http.get("services/services.php?brand=mtc").then(function(response) {
-      $scope.mtc = response.data;
+      var datamtc = response.data;
+      dataChart.push({"c" : [{"v" : "MTC", "f": null},{"v" : datamtc.last, "f": null}]});
+      $scope.mtc = datamtc;
     });
 
     $http.get("services/services.php?brand=btctoyou").then(function(response) {
-      $scope.btctoyou = response.data;
+      var databtctoyou = response.data;
+      dataChart.push({"c" : [{"v" : "btctoyou", "f": null},{"v" : databtctoyou.last, "f": null}]});
+      $scope.btctoyou = databtctoyou;
     });
 
     $http.get("services/services.php?brand=flowbtc").then(function(response) {
-      $scope.flowbtc = response.data;
+      var dataflowbtc = response.data;
+      dataChart.push({"c" : [{"v" : "flowbtc", "f": null},{"v" : dataflowbtc.last, "f": null}]});
+      $scope.flowbtc = dataflowbtc;
     });
 
     $http.get("services/services.php?brand=negociecoins").then(function(response) {
-      $scope.negociecoins = response.data;
+      var datanegociecoins = response.data;
+      dataChart.push({"c" : [{"v" : "negociecoins", "f": null},{"v" : datanegociecoins.last, "f": null}]});
+      $scope.negociecoins = datanegociecoins;
     });
+
+    exchangesChartService.data.rows = dataChart;
+    $scope.chart = exchangesChartService;
+
 
   }]);
