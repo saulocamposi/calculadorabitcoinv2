77a4bfeed845458d6ebc88a79674f042efab5401
saulocamposi <saulocamposi@gmail.com>
2016-06-16 20:46:53 -0300

    Add chartdate

--- js/controllers/polionexcontroller.js
+++ js/controllers/polionexcontroller.js
@@ -1,27 +1,33 @@
 var app = require('../manager-module.js');
 
-app.controller('polionexcontroller', ['$scope','$http', 'exchange', function($scope, $http, exchange) {
-    $scope.exchangeservice = exchange;
+app.controller('polionexcontroller', ['$scope','$http', 'polionexChartService', function($scope, $http, polionexChartService) {
+
+    var dataChart = [];
 
     $http.get("services/doge_polionex_services.php").then(function(response) {
-      $scope.doge = response.data;
+      var doge = response.data;
+      $scope.doge = doge;
+      dataChart.push({"c" : [{"v" : "Doge", "f": null},{"v" : doge.volume, "f": null}]});
     });
 
     $http.get("services/dash_polionex_services.php").then(function(response) {
-      $scope.dash = response.data;
+      var dash = response.data;
+      $scope.dash = dash;
+      dataChart.push({"c" : [{"v" : "Dash", "f": null},{"v" : dash.volume, "f": null}]});
     });
 
     $http.get("services/lite_polionex_services.php").then(function(response) {
-      $scope.lite = response.data;
+      var lite = response.data;
+      $scope.lite = lite;
+      dataChart.push({"c" : [{"v" : "Lite", "f": null},{"v" : lite.volume, "f": null}]});
     });
 
     $http.get("services/ether_polionex_services.php").then(function(response) {
-      $scope.ether = response.data;
+      var ether = response.data;
+      $scope.ether = ether;
+      dataChart.push({"c" : [{"v" : "FoxBit", "f": null},{"v" : ether.volume, "f": null}]});
     });
 
-
-  }])
-  .factory('exchange', ['$http', function($http) {
-    console.log($http);
-    return "hello";
+    polionexChartService.data.rows = dataChart;
+    $scope.chart = polionexChartService;
   }]);
