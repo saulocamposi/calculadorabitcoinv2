60b007b2b73623d0ca08434af60814484334881c
Saulo Campos <saulocamposi@gmail.com>
2016-07-20 00:42:38 -0300

    Fix change path to app

--- js/controllers/coinbrcontroller.js
+++ js/controllers/coinbrcontroller.js
@@ -2,8 +2,8 @@ var app = require('../manager-module.js');
 
 app.controller('coinbrcontroller', ['$scope','$http', function($scope, $http) {
 
-  $http.get("services/services.php?brand=coinbr").then(function(response) {
+  $http.get("app/services/services.php?brand=coinbr").then(function(response) {
     $scope.coinbr = response.data;
-  });  
+  });
 
   }]);
