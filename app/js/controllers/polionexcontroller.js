eb2ca4e8f9d8c2487dc6f9e422bf91d58a8b4611
Saulo Camposi <saulocamposi@gmail.com>
2017-01-05 17:59:25 -0200

    Change polionex debug

--- app/js/controllers/polionexcontroller.js
+++ app/js/controllers/polionexcontroller.js
@@ -1,40 +1,39 @@
 var app = require('../manager-module.js');
 
-
 var polionexcontroller = function($scope, $http, polionexChartService, googleChartApiPromise) {
 
     var dataChart = [];
+    console.log(polionexChartService);
 
     $http.get("app/services/doge_polionex_services.php").then(function(response) {
       var doge = response.data;
-      $scope.doge = doge;
       dataChart.push({"c" : [{"v" : "Doge", "f": null},{"v" : doge.volume, "f": null}]});
+      $scope.doge = doge;
     });
 
     $http.get("app/services/dash_polionex_services.php").then(function(response) {
       var dash = response.data;
-      $scope.dash = dash;
       dataChart.push({"c" : [{"v" : "Dash", "f": null},{"v" : dash.volume, "f": null}]});
+      $scope.dash = dash;
     });
 
     $http.get("app/services/lite_polionex_services.php").then(function(response) {
       var lite = response.data;
-      $scope.lite = lite;
       dataChart.push({"c" : [{"v" : "Lite", "f": null},{"v" : lite.volume, "f": null}]});
+      $scope.lite = lite;
+
     });
 
     $http.get("app/services/ether_polionex_services.php").then(function(response) {
       var ether = response.data;
-      $scope.ether = ether;
       dataChart.push({"c" : [{"v" : "FoxBit", "f": null},{"v" : ether.volume, "f": null}]});
+      $scope.ether = ether;
     });
 
-    console.log(dataChart);
     polionexChartService.data.rows = dataChart;
     $scope.chart = polionexChartService;
+
+    console.log(dataChart);
   }
-
-
-polionexcontroller.$inject = ['$scope', 'googleChartApiPromise'];
 
 app.controller('polionexcontroller', ['$scope','$http', 'polionexChartService', polionexcontroller ]);
