128bac6367f0897d89b5ae3e30f4a81d328dba73
Saulo Camposi <saulocamposi@gmail.com>
2017-02-17 13:27:00 -0200

    Fix using webpack server

--- app/js/controllers/bitinkacontroller.js
+++ app/js/controllers/bitinkacontroller.js
@@ -3,7 +3,8 @@ var app = require('../manager-module.js');
 app.controller('bitinkacontroller', ['$scope','$http','bitinkaChartService', function($scope, $http, bitinkaChartService) {
  var dataChart = [];
 
-    $http.get("app/services/bitinka_services.php").then(function(response) {
+    $http.get("http://localhost/calculadorabitcoinv2/app/services/bitinka_services.php").then(function(response) {
+
       var bitinka = response.data;
       $scope.bitinka = bitinka;
 
