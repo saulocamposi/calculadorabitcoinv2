8765a12d16ced932ad443422b3c5857122443172
Saulo Camposi <saulocamposi@gmail.com>
2017-02-23 21:10:00 -0300

    Updated endpoint to localhost

--- app/js/controllers/walletfoxbitcontroller.js
+++ app/js/controllers/walletfoxbitcontroller.js
@@ -2,11 +2,11 @@ var app = require('../manager-module.js');
 
 app.controller('walletfoxbitcontroller', ['$scope', '$http', function($scope, $http) {
 
-  $http.get("app/services/walletfoxbit_cold_services.php").then(function(response) {
+  $http.get("http://localhost/calculadorabitcoinv2/app/services/walletfoxbit_cold_services.php").then(function(response) {
     $scope.wfoxcold = response.data;
   });
 
-  $http.get("app/services/walletfoxbit_hot_services.php").then(function(response) {
+  $http.get("http://localhost/calculadorabitcoinv2/app/services/walletfoxbit_hot_services.php").then(function(response) {
     $scope.wfoxhot = response.data;
   });
 
