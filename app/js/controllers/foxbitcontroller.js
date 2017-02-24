0118173338957aa1802833635026035073241978
Saulo Camposi <saulocamposi@gmail.com>
2017-02-23 21:06:18 -0300

    Updated jsonp to get

--- app/js/controllers/foxbitcontroller.js
+++ app/js/controllers/foxbitcontroller.js
@@ -2,11 +2,11 @@ var app = require('../manager-module.js');
 
 app.controller('foxbitcontroller', ['$scope','$http', function($scope, $http) {
 
-    $http.get("app/services/orders_foxbit_services.php").then(function(response) {
+    $http.get("http://localhost/calculadorabitcoinv2/app/services/orders_foxbit_services.php").then(function(response) {
       $scope.foxbitorders = response.data;
     });
 
-    $http.jsonp("https://api.blinktrade.com/api/v1/BRL/trades?crypto_currency=BTC&callback=JSON_CALLBACK").then(function(response) {
+    $http.get("https://api.blinktrade.com/api/v1/BRL/trades?crypto_currency=BTC").then(function(response) {
       $scope.foxbittrades = response.data;
     });
 
