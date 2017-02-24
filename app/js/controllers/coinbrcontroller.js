3cb061e59c3b43c315c0dab58fe6e0d8d0b5c196
Saulo Camposi <saulocamposi@gmail.com>
2017-02-23 21:05:17 -0300

    Updated endpoint to localhost

--- app/js/controllers/coinbrcontroller.js
+++ app/js/controllers/coinbrcontroller.js
@@ -2,7 +2,7 @@ var app = require('../manager-module.js');
 
 app.controller('coinbrcontroller', ['$scope','$http', function($scope, $http) {
 
-  $http.get("app/services/services.php?brand=coinbr").then(function(response) {
+  $http.get("http://localhost/calculadorabitcoinv2/app/services/services.php?brand=coinbr").then(function(response) {
     $scope.coinbr = response.data;
   });
 
