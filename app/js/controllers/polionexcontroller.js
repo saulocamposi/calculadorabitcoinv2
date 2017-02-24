1e4cbb40033f7cf980759c26869301994c019897
Saulo Camposi <saulocamposi@gmail.com>
2017-02-23 21:06:43 -0300

    Updated endpoint to localhost

--- app/js/controllers/polionexcontroller.js
+++ app/js/controllers/polionexcontroller.js
@@ -5,26 +5,26 @@ var polionexcontroller = function($scope, $http, polionexChartService, googleCha
     var dataChart = [];
     console.log(polionexChartService);
 
-    $http.get("app/services/doge_polionex_services.php").then(function(response) {
+    $http.get("http://localhost/calculadorabitcoinv2/app/services/doge_polionex_services.php").then(function(response) {
       var doge = response.data;
       dataChart.push({"c" : [{"v" : "Doge", "f": null},{"v" : doge.volume, "f": null}]});
       $scope.doge = doge;
     });
 
-    $http.get("app/services/dash_polionex_services.php").then(function(response) {
+    $http.get("http://localhost/calculadorabitcoinv2/app/services/dash_polionex_services.php").then(function(response) {
       var dash = response.data;
       dataChart.push({"c" : [{"v" : "Dash", "f": null},{"v" : dash.volume, "f": null}]});
       $scope.dash = dash;
     });
 
-    $http.get("app/services/lite_polionex_services.php").then(function(response) {
+    $http.get("http://localhost/calculadorabitcoinv2/app/services/lite_polionex_services.php").then(function(response) {
       var lite = response.data;
       dataChart.push({"c" : [{"v" : "Lite", "f": null},{"v" : lite.volume, "f": null}]});
       $scope.lite = lite;
 
     });
 
-    $http.get("app/services/ether_polionex_services.php").then(function(response) {
+    $http.get("http://localhost/calculadorabitcoinv2/app/services/ether_polionex_services.php").then(function(response) {
       var ether = response.data;
       dataChart.push({"c" : [{"v" : "FoxBit", "f": null},{"v" : ether.volume, "f": null}]});
       $scope.ether = ether;
