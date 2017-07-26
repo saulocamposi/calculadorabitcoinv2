9ddb3b04b9d98cc21d94d8616dcc7b20b2af6f35
Saulo Camposi <saulocamposi@gmail.com>
2017-07-27 00:22:04 +0100

    Add create table

---
+++ ws_services/create_table_db.php
@@ -0,0 +1,31 @@
+<?php
+
+    include( dirname(__FILE__) . "/connection_db.php");
+
+    $connect = mysqli_connect($servername, $user, $password, $dbname);
+
+    if (!$connect) {
+      die("Connection failed: \r\n " . mysqli_connect_error() );
+    } else {
+      echo "Connection Sucess \r\n ";
+    }
+
+    if(mysqli_query($connect,$sql)){
+      echo "Success\r\n";
+    } else {
+      echo "Error insert \r\n" . mysqli_error($connect);
+    };
+
+    if(mysqli_close($connect)){
+      echo "Success Close DB\r\n";
+    } else {
+      echo "Error insert \r\n" . mysqli_error($connect);
+    };
+
+
+
+
+
+    //echo $poloniex =  file_get_contents("https://poloniex.com/public?command=returnTicker");
+
+?>
