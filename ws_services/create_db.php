bc1894e1bd48656f6b4268e53027eac4c970f63b
Saulo Camposi <saulocamposi@gmail.com>
2017-07-26 23:51:31 +0100

    Add create db

---
+++ ws_services/create_db.php
@@ -0,0 +1,28 @@
+<?php
+
+    include( dirname(__FILE__) . "/connection_db.php");
+
+    $connect = mysqli_connect($servername, $user, $password);
+
+    if (!$connect) {
+      die("Connection failed: " . mysqli_connect_error() );
+    } else {
+      echo "Connection Sucess";
+    }
+
+    $sql = "CREATE DATABASE myDB";
+    //$sql = "insert into cronlog (1,1)";
+    if(mysqli_query($connect,$sql)){
+      echo "Success";
+    } else {
+      echo "Error insert" + mysqli_error($connect);
+    };
+
+
+    mysqli_close($connect);
+
+
+
+    //echo $poloniex =  file_get_contents("https://poloniex.com/public?command=returnTicker");
+
+?>
