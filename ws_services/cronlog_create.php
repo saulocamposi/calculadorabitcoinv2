78924249b15adfb2ca17cf69e3f109c9bd266fc6
Saulo Camposi <saulocamposi@gmail.com>
2017-07-27 03:21:40 +0100

    Delete files were moved to model core

--- ws_services/cronlog_create.php
+++
@@ -1,5 +0,0 @@
-<?php
-  $sql = "CREATE TABLE cronlog ( hour int , transaction int )";
-  
-  include( dirname(__FILE__) . "/create_table_db.php");
-?>
--- ws_services/ticker_create.php
+++
@@ -1,4 +0,0 @@
-<?php
-  $sql = "CREATE TABLE ticker ( hour int , transaction int )";
-  include( dirname(__FILE__) . "/create_table_db.php");
-?>
