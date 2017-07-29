3ead720d3a4db5e1739ae45791e4063be6aaa862
Saulo Camposi <saulocamposi@gmail.com>
2017-07-29 18:45:08 +0100

    Delete valeu from root path

--- app/js/controllers/exchangescontroller.js
+++ app/js/controllers/exchangescontroller.js
@@ -4,31 +4,33 @@ app.controller('exchangescontroller', ['$scope','$http', 'exchangesChartService'
 
   var dataChart = [];
 
-    $http.get("backend/ws/services/services.php?brand=fox").then(function(response) {
+  var root_path = "backend/ws/services/";
+
+    $http.get( root_path + "services.php?brand=fox").then(function(response) {
       var datafoxbit = response.data;
       dataChart.push({"c" : [{"v" : "FoxBit", "f": null},{"v" : datafoxbit.last, "f": null}]});
       $scope.foxbit = datafoxbit;
     });
 
-    $http.get("backend/ws/services/services.php?brand=mtc").then(function(response) {
+    $http.get( root_path + "services.php?brand=mtc").then(function(response) {
       var datamtc = response.data;
       dataChart.push({"c" : [{"v" : "MTC", "f": null},{"v" : datamtc.last, "f": null}]});
       $scope.mtc = datamtc;
     });
 
-    $http.get("backend/ws/services/services.php?brand=btctoyou").then(function(response) {
+    $http.get( root_path + "services.php?brand=btctoyou").then(function(response) {
       var databtctoyou = response.data;
       dataChart.push({"c" : [{"v" : "btctoyou", "f": null},{"v" : databtctoyou.last, "f": null}]});
       $scope.btctoyou = databtctoyou;
     });
 
-    $http.get("backend/ws/services/services.php?brand=flowbtc").then(function(response) {
+    $http.get( root_path + "services.php?brand=flowbtc").then(function(response) {
       var dataflowbtc = response.data;
       dataChart.push({"c" : [{"v" : "flowbtc", "f": null},{"v" : dataflowbtc.last, "f": null}]});
       $scope.flowbtc = dataflowbtc;
     });
 
-    $http.get("backend/ws/services/services.php?brand=negociecoins").then(function(response) {
+    $http.get( root_path + "services.php?brand=negociecoins").then(function(response) {
       var datanegociecoins = response.data;
       dataChart.push({"c" : [{"v" : "negociecoins", "f": null},{"v" : datanegociecoins.last, "f": null}]});
       $scope.negociecoins = datanegociecoins;
@@ -36,6 +38,5 @@ app.controller('exchangescontroller', ['$scope','$http', 'exchangesChartService'
 
     exchangesChartService.data.rows = dataChart;
     $scope.chart = exchangesChartService;
-
 
   }]);
