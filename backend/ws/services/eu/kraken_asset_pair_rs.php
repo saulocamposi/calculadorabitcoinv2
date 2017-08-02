58938ebd511e3f36229d412ed31eee0c19b5eb9f
Saulo Camposi <saulocamposi@gmail.com>
2017-08-02 04:47:54 +0100

    Update kraken pairs

--- backend/ws/services/eu/kraken_asset_pair_rs.php
+++ backend/ws/services/eu/kraken_asset_pair_rs.php
@@ -1,4 +1,21 @@
 <?php
-   $service =  file_get_contents("https://api.kraken.com/0/public/AssetPairs");
-   echo $service;
+   $service =  json_decode(file_get_contents("https://api.kraken.com/0/public/AssetPairs"));
+
+   $count = 0;
+
+   function getUrlsPairs($service, $count){
+     foreach ($service->result as $key => $value) {
+       echo $key . "</br>";
+       $count++;
+     }
+     echo $count;
+   }
+
+
+   getUrlsPairs($service,$count);
+
+
+
+
+
 ?>
