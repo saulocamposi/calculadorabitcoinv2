d382fc748680777eea5ea293d4e223e679d28e27
Saulo Camposi <saulocamposi@gmail.com>
2017-08-09 21:34:32 +0100

    Add bootstrap-database

---
+++ backend/model/core/bootstrap_database.php
@@ -0,0 +1,15 @@
+<?php
+
+  include( dirname(__FILE__) . "/drop_db.php");
+  include( dirname(__FILE__) . "/create_db.php");
+
+
+  include( dirname(__FILE__) . "/cronlog_create.php");
+
+  include( dirname(__FILE__) . "/ticker_create.php");
+  include( dirname(__FILE__) . "/ticker_bitcointoyou_create.php");
+  include( dirname(__FILE__) . "/ticker_flowbtc_create.php");
+  include( dirname(__FILE__) . "/ticker_foxbit_create.php");
+  include( dirname(__FILE__) . "/ticker_mbtc_create.php");
+
+ ?>
