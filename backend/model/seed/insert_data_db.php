7aa66b176f7f3812ab1419dde1c6d1159757c407
saulocamposi <saulocamposi@gmail.com>
2017-08-25 21:09:13 +0100

    Update log seed

--- backend/model/seed/insert_data_db.php
+++ backend/model/seed/insert_data_db.php
@@ -9,7 +9,8 @@
     $mysqltime = date ($time);
 
     $sql = "INSERT INTO cronlog ( id, transaction, created_at ) values ( NULL, $transaction , $mysqltime );";
-echo $sql;
-    include( dirname(__FILE__ , 2) . "/core//pool_db.php");
+
+    echo $sql . "\r\n";
+    include( dirname( __FILE__ , 2 ) . "/core//pool_db.php");
 
 ?>
