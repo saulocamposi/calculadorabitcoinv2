a0c4a8ac895c57db7aaf063dc204b14944d87872
saulocamposi <saulocamposi@gmail.com>
2017-08-31 20:15:54 +0100

    Update remove time variable

--- backend/model/seed/insert_ticker_bitcointoyou.php
+++ backend/model/seed/insert_ticker_bitcointoyou.php
@@ -4,18 +4,18 @@
 
     $message = "INSERTED ROW";
 
-    $time = time();
-    $transaction = 2;
-    $mysqltime = date ($time);
-
     $sql = "INSERT INTO ticker_bitcointoyou (
             id,
-            transaction,
+            volume,
+            min,
+            max,
             created_at )
             VALUES (
               NULL,
-              $transaction,
-              $mysqltime );";
+              $ticker->vol,
+              $ticker->low,
+              $ticker->high,
+              null );";
 
     include( dirname( __FILE__ , 2 ) . "/core//pool_db.php");
 
