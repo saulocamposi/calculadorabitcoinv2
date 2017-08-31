524b32f73cddb9528834bef026951354c16c60f3
saulocamposi <saulocamposi@gmail.com>
2017-08-31 01:58:13 +0100

    Update flag log

--- backend/model/core/pool_db.php
+++ backend/model/core/pool_db.php
@@ -11,19 +11,33 @@
     if ( !$connect ) {
       die("Connection failed:\r\n " . mysqli_connect_error() );
     } else {
-      echo "Connection Sucess\r\n";
+      if ($log) {
+        echo "Connection Sucess\r\n";
+      }
+
     }
 
     if(mysqli_query( $connect, $sql )){
-      echo $message . "\r\n";
+      if($log){
+        echo $message . "\r\n";
+        echo $sql . "\r\n";
+      }
     } else {
-      echo "Error insert\r\n" . mysqli_error( $connect );
+      if ($log) {
+        echo $sql . "\r\n";
+        echo "Error insert\r\n" . mysqli_error( $connect ). "\r\n";
+      }
+
     };
 
     if(mysqli_close( $connect )){
-      echo "Success Close DB\r\n";
-    } else {
-      echo "Error CloseDB \r\n" . mysqli_error( $connect );
-    };
+      if($log){
+        echo "Success Close DB\r\n";
+      }
 
+    } else {
+      if ($log) {
+        echo "Error CloseDB \r\n" . mysqli_error( $connect ). "\r\n";
+      }
+    };
 ?>
