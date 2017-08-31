ce30d77c3eb16a28c0196be42cdacd36459f874e
saulocamposi <saulocamposi@gmail.com>
2017-08-31 20:16:21 +0100

    Update remove time variable, identation query

--- backend/model/seed/insert_ticker_mbtc.php
+++ backend/model/seed/insert_ticker_mbtc.php
@@ -4,22 +4,18 @@
 
     $message = "INSERTED ROW";
 
-    $time = time();
-    $transaction = 2;
-    $mysqltime = date ($time);
-
-    $sql = "INSERT INTO ticker_mtc (
-      id,
-      volume,
-      min,
-      max,
-      created_at )
-      VALUES (
-        NULL,
-        $foxbit->vol,
-        $foxbit->low,
-        $foxbit->high,
-        null );";
+    $sql = "INSERT INTO ticker_mbtc (
+            id,
+            volume,
+            min,
+            max,
+            created_at )
+            VALUES (
+              NULL,
+              $ticker->vol,
+              $ticker->low,
+              $ticker->high,
+              null );";
     include( dirname( __FILE__ , 2 ) . "/core//pool_db.php");
 
 ?>
