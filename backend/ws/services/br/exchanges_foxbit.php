1e5acf9f0afc8b5e786eb79c6610b52fbc53d56d
saulocamposi <saulocamposi@gmail.com>
2017-08-31 20:13:51 +0100

    Fix include path

--- backend/ws/services/br/exchanges_foxbit.php
+++ backend/ws/services/br/exchanges_foxbit.php
@@ -2,6 +2,6 @@
 
      $ticker =  json_decode(file_get_contents("https://api.blinktrade.com/api/v1/BRL/ticker?crypto_currency=BTC"));
 
-     include( dirname( __FILE__ , 3 ) . "/model/seed/insert_ticker_foxbit.php");
+     include( dirname( __FILE__ , 4 ) . "/model/seed/insert_ticker_foxbit.php");
 
 ?>
