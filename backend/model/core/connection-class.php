cd091157dc9c11cfb36ca28076b7a9731701672a
saulocamposi <saulocamposi@gmail.com>
2017-09-01 22:41:54 +0100

    Update - identation

--- backend/model/core/connection-class.php
+++ backend/model/core/connection-class.php
@@ -2,19 +2,23 @@
 
   include( dirname(__FILE__) . "/connection_db.php");
 
-  class Connection {
+  class Connection
+  {
 
     private $connect;
 
-    __construct( $servername, $database, $user, $password ){
+    __construct( $servername, $database, $user, $password )
+    {
       $this->connect = getConnection( $servername, $database, $user, $password );
     }
 
-    function destroyConnection($connect){
+    function destroyConnection($connect)
+    {
       mysqli_close( $connect )
     }
 
-    function getConnection( $servername, $database, $user, $password ){
+    function getConnection( $servername, $database, $user, $password )
+    {
       return mysqli_connect( $servername, $user, $password );
     }
 
