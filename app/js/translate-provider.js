bd6a0f1f2bcd948496ae6c1964615a8ae89cfd74
saulocamposi <saulocamposi@gmail.com>
2017-09-01 22:16:26 +0100

    Update translate fields

--- app/js/translate-provider.js
+++ app/js/translate-provider.js
@@ -3,14 +3,40 @@ var app = require('./manager-module.js');
   app.config(['$translateProvider', function($translateProvider) {
 
     var translations_br = {
-      'wallet' : 'Carteiras Foxbit',
-      'bids' : 'Ordens'
+      'wallet' : 'Foxbit Wallet',
+      'bids' : 'Bids',
+      'exchanges' : 'Exchanges',
+      'paircoin' : 'PairCoin',
+      'exchanges' : 'Exchanges',
+      'polionex_title' : 'Polionex Exchange - Altcoins Market',
+      'volume' : 'Volume',
+      'lasttrade' : 'Last Trade',
+      'price' : 'Price',
+      'companies' : 'Companies',
+      'volume' : 'Volume',
+      'highvalue' : 'High Value',
+      'lowvalue' : 'Low Value',
+      'value' : 'Value'
     }
 
     var translations_en = {
-      'wallet' : 'Foxbit Wallets',
-      'bids' : 'Bids'
+      'wallet' : 'Foxbit Wallet',
+      'bids' : 'Bids',
+      'exchanges' : 'Exchanges',
+      'paircoin' : 'PairCoin',
+      'exchanges' : 'Exchanges',
+      'polionex_title' : 'Polionex Exchange - Altcoins Market',
+      'volume' : 'Volume',
+      'lasttrade' : 'Last Trade',
+      'price' : 'Price',
+      'companies' : 'Companies',
+      'volume' : 'Volume',
+      'highvalue' : 'High Value',
+      'lowvalue' : 'Low Value',
+      'value' : 'Value'
     }
+
+
 
     $translateProvider.translations('en', translations_en);
     $translateProvider.translations('br', translations_br);
