d51bd1a5d1302982f36770cc3bc10b8d9416bd8b
saulocamposi <saulocamposi@gmail.com>
2017-09-01 21:44:12 +0100

    Add base test

---
+++ backend/test/case-test.php
@@ -0,0 +1,25 @@
+<?php
+  use PHPUnit\Framework\TestCase;
+
+  /**
+   *
+   */
+  class CaseTest extends TestCase
+  {
+
+    public function testPushAndPop()
+    {
+      $stack = [];
+      $this->assertEquals( 0, count( $stack ) );
+      array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+  }
+
+
+
+ ?>
