e70024ea8b81edf2212b1ecbc073643dd79a24c1
saulocamposi <saulocamposi@gmail.com>
2017-09-03 23:23:53 +0100

    Update -- select query

--- backend/model/selects/ticker_foxbit_select.php
+++ backend/model/selects/ticker_foxbit_select.php
@@ -8,10 +8,10 @@
 
   $max = $result->fetch_assoc();
 
-  $sql = "SELECT id, volume, min, max, created_at from  ticker_foxbit where id = " . $max['max(id)'];
+  $sql = "SELECT id, vol, low, high, created_at from  ticker_foxbit where id = " . $max['max(id)'];
 
   include( dirname( __FILE__ , 2 ) . "/core/pool_db.php");
 
-  $row = $result->fetch_assoc();  
+  $row = $result->fetch_assoc();
 
 ?>
