ca333c1b563d7b6624fb3d10ed0d492f6bb85cc4
saulocamposi <saulocamposi@gmail.com>
2017-09-04 00:14:35 +0100

    Update -- flag create db

--- backend/model/automatization/bootstrap_database.php
+++ backend/model/automatization/bootstrap_database.php
@@ -1,8 +1,11 @@
 <?php
 
+  $create_db = true;
+
   include( dirname( __FILE__ , 2) . "/core/drop_db.php");
   include( dirname( __FILE__ , 2) . "/core/create_db.php");
 
+  $create_db = false;
 
   include( dirname( __FILE__ , 2 ) . "/migrations/cronlog_create.php");
   include( dirname( __FILE__ , 2 ) . "/migrations/ticker_create.php");
@@ -10,5 +13,6 @@
   include( dirname( __FILE__ , 2 ) . "/migrations/ticker_flowbtc_create.php");
   include( dirname( __FILE__ , 2 ) . "/migrations/ticker_foxbit_create.php");
   include( dirname( __FILE__ , 2 ) . "/migrations/ticker_mbtc_create.php");
+  include( dirname( __FILE__ , 2 ) . "/migrations/ticker_coinbr_create.php");
 
  ?>
