986e405c6e66540a546b67abb36f3e55138b1a2d
saulocamposi <saulocamposi@gmail.com>
2017-09-04 00:12:25 +0100

    Update -- selected concluded

--- backend/model/selects/ticker_bitcointoyou_select.php
+++ backend/model/selects/ticker_bitcointoyou_select.php
@@ -2,10 +2,16 @@
 
   $message = "ENTITY TICKER BITCOINTOYOU SELECTED";
 
-  $sql = "SELECT id, volume, min, max, created_at from  ticker_bitcointoyou";
+  $sql = "SELECT max(id) from ticker_bitcointoyou";
 
   include( dirname( __FILE__ , 2 ) . "/core/pool_db.php");
 
-  print_r( $result );
+  $max = $result->fetch_assoc();
+
+  $sql = "SELECT id, vol, low, high, last, created_at from ticker_bitcointoyou where id = " . $max['max(id)'];
+
+  include( dirname( __FILE__ , 2 ) . "/core/pool_db.php");
+
+  $row = $result->fetch_assoc();
 
 ?>
