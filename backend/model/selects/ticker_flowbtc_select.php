ac9d92796e1a0269a05a11e06b2fede53f222d66
saulocamposi <saulocamposi@gmail.com>
2017-09-04 05:36:21 +0100

    Add -- flowbtc select

---
+++ backend/model/selects/ticker_flowbtc_select.php
@@ -0,0 +1,17 @@
+<?php
+
+  $message = "ENTITY TICKER FLOWBTC SELECTED";
+
+  $sql = "SELECT max(id) from ticker_flowbtc";
+
+  include( dirname( __FILE__ , 2 ) . "/core/pool_db.php");
+
+  $max = $result->fetch_assoc();
+
+  $sql = "SELECT id, vol, low, high, last, created_at from ticker_flowbtc where id = " . $max['max(id)'];
+
+  include( dirname( __FILE__ , 2 ) . "/core/pool_db.php");
+
+  $row = $result->fetch_assoc();
+
+?>
