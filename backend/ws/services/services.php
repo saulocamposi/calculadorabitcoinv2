f06fc62afa6e3e7b4ef4af9b57cf99f6d3628204
saulocamposi <saulocamposi@gmail.com>
2017-09-04 05:44:44 +0100

    Update -- refactore

--- backend/ws/services/services.php
+++ backend/ws/services/services.php
@@ -1,42 +1,46 @@
 <?php
 
-  if($_GET['brand'] == "fox"){
+  if( $_GET['brand'] == "fox" ){
 
     include( dirname( __FILE__ , 3 ) . "/model/selects/ticker_foxbit_select.php");
-
-    echo json_encode($row);
+    echo json_encode( $row );
 
     //echo $foxbit =  file_get_contents("https://api.blinktrade.com/api/v1/BRL/ticker?crypto_currency=BTC");
   }
 
-  if($_GET['brand'] == "btctoyou"){
-    try {
-      echo $bitcointoyou =  file_get_contents("https://www.bitcointoyou.com/api/ticker.aspx");
-    }
-    catch (Exception $e) {
-      echo $e->getMessage();
-    }
-  }
+  if( $_GET['brand'] == "btctoyou" ){
 
-  if($_GET['brand'] == "negociecoins"){
-    echo $negociecoins =  file_get_contents("http://www.negociecoins.com.br/api/v3/btcbrl/ticker");
-  }
-
-  if($_GET['brand'] == "flowbtc"){
-    echo $flowbtc =  file_get_contents("https://api.flowbtc.com:8400/GetTicker/BTCBRL/");
-  }
-
-  if($_GET['brand'] == "mtc"){
-    try {
-      echo $mtc =  file_get_contents("https://www.mercadobitcoin.net/api/ticker");
-    }
-    catch (Exception $e) {
-      echo $e->getMessage();
-    }
+      include( dirname( __FILE__ , 3 ) . "/model/selects/ticker_bitcointoyou_select.php" );
+      echo json_encode( $row );
+      //echo $bitcointoyou =  file_get_contents("https://www.bitcointoyou.com/api/ticker.aspx");
 
   }
 
-  if($_GET['brand'] == "coinbr"){
+  if( $_GET['brand'] == "negociecoins" ){
+    echo $negociecoins =  file_get_contents( "http://www.negociecoins.com.br/api/v3/btcbrl/ticker" );
+  }
+
+  if( $_GET['brand'] == "flowbtc" ){
+
+    include( dirname( __FILE__ , 3 ) . "/model/selects/ticker_flowbtc_select.php");
+    echo json_encode( $row );
+    //echo $flowbtc =  file_get_contents("https://api.flowbtc.com:8400/GetTicker/BTCBRL/");
+  }
+
+  if( $_GET['brand'] == "mbtc" ){
+    //try {
+
+      include( dirname( __FILE__ , 3 ) . "/model/selects/ticker_mbtc_select.php");
+      echo json_encode( $row );
+      //echo $mtc =  file_get_contents("https://www.mercadobitcoin.net/api/ticker");
+    //}
+    //catch ( Exception $e ) {
+    //  echo $e->getMessage();
+    //}
+
+  }
+
+  if( $_GET['brand'] == "coinbr" ){
     echo $coinbr =  file_get_contents("https://www.coinbr.net/pubticker");
   }
 ?>
