4dad42b6805f5d143f4eab5a0dbfd5b2cb5becdd
saulocamposi <saulocamposi@gmail.com>
2017-09-05 05:26:03 +0100

    Update - methods refactory

--- backend/model/core/active-record-class.php
+++ backend/model/core/active-record-class.php
@@ -6,7 +6,44 @@ class ActiveRecord {
 
     }
 
-  function getEntity (){
+function getConnection(){
+  if ( $this->create_db ) {
+    $connect = mysqli_connect( $this->servername, $this->user, $this->password );
+  } else {
+    $connect = mysqli_connect( $this->servername, $this->user, $this->password, $this->dbname );
+  }
+
+  if ( !$connect ) {
+    die("Connection failed:\r\n " . mysqli_connect_error() );
+  } else {
+    if ($log) {
+      echo "Connection Sucess\r\n";
+    }
+  }
+}
+
+function getEntity (){
+
+    if( $result = mysqli_query( $connect, $sql ) ){
+      if( $log ){
+
+        echo $message . "\r\n";
+        if( $sql_log ) {
+          echo $sql . "\r\n";
+        }
+
+      }
+    } else {
+      if ( $log ) {
+        if( $sql_log ) {
+          echo $sql . "\r\n";
+        }
+
+        echo "Error query\r\n" . mysqli_error( $connect ). "\r\n";
+      }
+
+    };
+
 
   }
 
@@ -21,5 +58,19 @@ class ActiveRecord {
   function createEntity(){
 
   }
+
+  function destroyConnection(){
+    if(mysqli_close( $connect )){
+      if($log){
+        echo "Success Close DB\r\n";
+      }
+
+    } else {
+      if ($log) {
+        echo "Error CloseDB \r\n" . mysqli_error( $connect ). "\r\n";
+      }
+    };
+  }
+
 }
 ?>
