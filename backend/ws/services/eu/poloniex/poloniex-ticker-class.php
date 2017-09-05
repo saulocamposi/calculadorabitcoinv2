35887fd6dc537e47a1ec108b26b6007ec57d44f1
saulocamposi <saulocamposi@gmail.com>
2017-09-05 05:26:42 +0100

    Update - methods refactory, persist method added

--- backend/ws/services/eu/poloniex/poloniex-ticker-class.php
+++ backend/ws/services/eu/poloniex/poloniex-ticker-class.php
@@ -2,17 +2,19 @@
 
 class PoloniexTicker
 {
+
   private $endpoint = "https://poloniex.com/public?command=returnTicker";
   private $ticker;
 
-  function __construct( $endpoint )
+  function __construct( $endpoint /*, $daoPoloniex */)
   {
 
     $this->endpoint = $endpoint;
 
   }
 
-  function checkEndPoint() {
+  function checkEndPoint()
+  {
     try {
 
       $this->ticker = file_get_contents( $this->endpoint );
@@ -25,31 +27,44 @@ class PoloniexTicker
     }
   }
 
-  function getTicker(){
-    return json_decode($this->ticker);
+  function getTicker()
+  {
+    return json_decode( $this->ticker );
   }
 
-  function countTickerElements($ticker){
+  function countTickerElements ( $ticker )
+  {
 
     $count = 0;
 
-    foreach ($ticker as $key => $value) {
+    foreach ( $ticker as $key => $value ) {
       $count = $count + 1;
     }
 
     return $count;
 
   }
+
+  function persist ( $ticker )
+  {
+
+  }
+
 }
 
 $endpoint = "https://poloniex.com/public?command=returnTicker";
-$poloniex = new PoloniexTicker("https://poloniex.com/public?command=returnTicker");
+$poloniex = new PoloniexTicker( "https://poloniex.com/public?command=returnTicker" );
 
 print $poloniex->checkEndPoint();
 $ticker = $poloniex->getTicker();
 
-print $count = $poloniex->countTickerElements($ticker);
+print $count = $poloniex->countTickerElements( $ticker );
 
-print_r ( each($ticker) );
+print_r ( each( $ticker ) );
+
+
+
+
+
 
 ?>
