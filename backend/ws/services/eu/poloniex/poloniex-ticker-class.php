47e1eed9fbff4cc90094d59c83892ab6582bad9d
saulocamposi <saulocamposi@gmail.com>
2017-09-05 03:50:15 +0100

    Add -- poloniex-ticker-class

---
+++ backend/ws/services/eu/poloniex/poloniex-ticker-class.php
@@ -0,0 +1,49 @@
+<?php
+
+class PoloniexTicker
+{
+  private $endpoint = "https://poloniex.com/public?command=returnTicker";
+  private $ticker;
+
+  function __construct( $endpoint )
+  {
+    $this->endpoint = $endpoint;
+  }
+
+  function checkEndPoint() {
+    try {
+      $this->ticker = file_get_contents( $this->endpoint );
+      return true;
+    } catch ( Exception $e ) {
+      return false;
+    }
+  }
+
+  function getTicker(){
+    return json_decode($this->ticker);
+  }
+
+  function countTickerElements($ticker){
+
+    $count = 0;
+
+    foreach ($ticker as $key => $value) {
+      $count = $count + 1;
+    }
+
+    return $count;
+
+  }
+}
+
+$endpoint = "https://poloniex.com/public?command=returnTicker";
+$poloniex = new PoloniexTicker("https://poloniex.com/public?command=returnTicker");
+
+print $poloniex->checkEndPoint();
+$ticker = $poloniex->getTicker();
+
+print $count = $poloniex->countTickerElements($ticker);
+
+print_r( each($ticker) );
+
+?>
