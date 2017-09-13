1df6303f0fa053a4eaf2042732936f9cfa54bcf4
saulocamposi <saulocamposi@gmail.com>
2017-09-13 03:53:14 +0100

    Add -- insert ticker poloniex

---
+++ backend/model/seed/insert_ticker_poloniex.php
@@ -0,0 +1,27 @@
+<?php
+
+    print_r($ticker);
+
+    $message = "INSERTED ROW";
+
+    $sql = "INSERT INTO ticker_mbtc (
+              id ,
+              vol ,
+              low ,
+              high ,
+              last ,
+              pair ,
+              created_at
+            )
+            VALUES (
+              NULL,
+              $ticker->vol,
+              $ticker->low,
+              $ticker->high,
+              $ticker->last,
+              $ticker->pair,
+              null );";
+
+    include( dirname( __FILE__ , 2 ) . "/core//pool_db.php");
+
+?>
