104af7bcbeccf00fbde8425664c920e1368703f2
saulocamposi <saulocamposi@gmail.com>
2017-09-14 07:06:30 +0100

    Update remove console log

--- backend/ws/services/eu/poloniex/poloniex-ticker-class.php
+++ backend/ws/services/eu/poloniex/poloniex-ticker-class.php
@@ -62,13 +62,13 @@ class PoloniexTicker
 
 }
 
-include( dirname(__FILE__, 5) . "/model/selects/ticker_poloniex_select.php" );
+//include( dirname(__FILE__, 5) . "/model/selects/ticker_poloniex_select.php" );
 
-$endpoint = "https://poloniex.com/public?command=returnTicker";
-$poloniex = new PoloniexTicker( "https://poloniex.com/public?command=returnTicker" );
+//$endpoint = "https://poloniex.com/public?command=returnTicker";
+//$poloniex = new PoloniexTicker( "https://poloniex.com/public?command=returnTicker" );
 
-echo "Number of elements is: " . $poloniex->countTickerElements() . "\r\n";
+//echo "Number of elements is: " . $poloniex->countTickerElements() . "\r\n";
 
-print_r (  $poloniex->getTicker()  );
+//print_r (  $poloniex->getTicker()  );
 
 ?>
