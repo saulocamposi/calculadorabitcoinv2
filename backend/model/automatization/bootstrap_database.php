02d19143cbc9627c2179d76b49ebe7f3f1cf54d3
saulocamposi <saulocamposi@gmail.com>
2017-09-15 05:00:24 +0100

    Udpate -- split automatization between migration and create db

--- backend/model/automatization/bootstrap_database.php
+++ backend/model/automatization/bootstrap_database.php
@@ -1,18 +1,10 @@
 <?php
 
+  $log = true;
+  $sql_log = true;
   $create_db = true;
 
   include( dirname( __FILE__ , 2) . "/core/drop_db.php");
   include( dirname( __FILE__ , 2) . "/core/create_db.php");
-
-  $create_db = false;
-
-  include( dirname( __FILE__ , 2 ) . "/migrations/cronlog_create.php");
-  include( dirname( __FILE__ , 2 ) . "/migrations/ticker_create.php");
-  include( dirname( __FILE__ , 2 ) . "/migrations/ticker_bitcointoyou_create.php");
-  include( dirname( __FILE__ , 2 ) . "/migrations/ticker_flowbtc_create.php");
-  include( dirname( __FILE__ , 2 ) . "/migrations/ticker_foxbit_create.php");
-  include( dirname( __FILE__ , 2 ) . "/migrations/ticker_mbtc_create.php");
-  include( dirname( __FILE__ , 2 ) . "/migrations/ticker_coinbr_create.php");
 
  ?>
