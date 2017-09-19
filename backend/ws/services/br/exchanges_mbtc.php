5d8aed39d355940a5fe0d40e21d6bf97ff113ed7
saulocamposi <saulocamposi@gmail.com>
2017-09-19 06:56:21 +0100

    Fix -- ticker response name

--- backend/ws/services/br/exchanges_mbtc.php
+++ backend/ws/services/br/exchanges_mbtc.php
@@ -1,6 +1,8 @@
 <?php
 
-     $ticker =  json_decode(file_get_contents("https://www.mercadobitcoin.net/api/ticker"));
+     $response =  json_decode(file_get_contents("https://www.mercadobitcoin.net/api/ticker"));
+
+     $ticker = $response->ticker;
 
      include( dirname( __FILE__ , 4 ) . "/model/seed/insert_ticker_mbtc.php");
 
