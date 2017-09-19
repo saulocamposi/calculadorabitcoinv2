264a723341be190611fdb94753fd11116b79159b
saulocamposi <saulocamposi@gmail.com>
2017-09-19 06:54:38 +0100

    Fix -- ticker response name

--- backend/ws/services/br/exchanges_bitcointoyou.php
+++ backend/ws/services/br/exchanges_bitcointoyou.php
@@ -1,6 +1,8 @@
 <?php
 
-     $ticker =  json_decode(file_get_contents("https://www.bitcointoyou.com/api/ticker.aspx"));
+     $response =  json_decode(file_get_contents("https://www.bitcointoyou.com/api/ticker.aspx"));
+
+     $ticker = $response->ticker;
 
      include( dirname( __FILE__ , 4 ) . "/model/seed/insert_ticker_bitcointoyou.php");
 
