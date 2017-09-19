adae9c04153ed7309c4c8c0060f8a56c71467e0e
saulocamposi <saulocamposi@gmail.com>
2017-09-19 04:22:37 +0100

    Add service poloniex

---
+++ backend/ws/services/eu/poloniex/poloniex-task-select.php
@@ -0,0 +1,12 @@
+<?php
+
+include (dirname(__FILE__) . "/poloniex-ticker-class.php");
+
+$endpoint = "https://poloniex.com/public?command=returnTicker";
+$conn = new Connection( $servername, $dbname, $user, $password );
+$activeRecord = new ActiveRecord($conn);
+$poloniex = new PoloniexTicker($endpoint, $activeRecord);
+//$ticker = $poloniex->getLastTicker();
+$ticker = $poloniex->getAllLastMinuteTicker();
+print_r($ticker);
+?>
