cf628eceee19ee1f6c7a2c686c24d8ba7603e41f
saulocamposi <saulocamposi@gmail.com>
2017-09-27 03:08:57 +0100

    Update -- remove require to autoload

--- backend/test/connection-class-test.php
+++ backend/test/connection-class-test.php
@@ -2,9 +2,6 @@
 
   use PHPUnit\Framework\TestCase;
 
-  include( dirname(__FILE__) . "/connection_db.php");
-  include( dirname(__FILE__) . "/connection-class.php");
-
   class ConnectionTest extends TestCase
   {
 
@@ -18,7 +15,7 @@
       $conn = new Connection( $servername, $dbname, $user, $password );
 
       print_r($conn);
-      
+
       $this->assertTrue($conn->getConnection());
     }
 
