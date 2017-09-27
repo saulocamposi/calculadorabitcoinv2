c22c292d6d8f4476b7a80c680f4a38c0c058c6d5
saulocamposi <saulocamposi@gmail.com>
2017-09-27 02:36:55 +0100

    Update -- root path for foxbit

--- app/js/controllers/foxbitcontroller.js
+++ app/js/controllers/foxbitcontroller.js
@@ -2,7 +2,7 @@ var app = require('../manager-module.js');
 
 app.controller('foxbitcontroller', ['$scope','$http', function($scope, $http) {
 
-    var root_path = "backend/src/ws/services/";
+    var root_path = "backend/src/ws/services/foxbit/";
 
     $http.get(root_path + "orders_foxbit_services.php").then(function(response) {
       $scope.foxbitorders = response.data;
