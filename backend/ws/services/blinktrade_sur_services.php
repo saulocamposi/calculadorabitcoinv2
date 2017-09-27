d1d35a54a22694be860f86c67cdbf3d7fef8c5e5
saulocamposi <saulocamposi@gmail.com>
2017-09-27 01:33:33 +0100

    Delete -- move ws to src

--- backend/ws/services/blinktrade_sur_services.php
+++
@@ -1,5 +0,0 @@
-<?php
-   $service =  file_get_contents("https://api.blinktrade.com/api/v1/VEF/ticker?crypto_currency=BTC");
-
-   echo $service;
-?>
--- backend/ws/services/br/exchanges_bitcointoyou.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-     $response =  json_decode(file_get_contents("https://www.bitcointoyou.com/api/ticker.aspx"));
-
-     $ticker = $response->ticker;
-
-     include( dirname( __FILE__ , 4 ) . "/model/seed/insert_ticker_bitcointoyou.php");
-
-?>
