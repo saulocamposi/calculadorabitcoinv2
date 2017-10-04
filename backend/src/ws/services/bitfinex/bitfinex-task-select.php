48b85ce9572fb4b4e65a3956b7869236189de2c7
saulocamposi <saulocamposi@gmail.com>
2017-10-04 14:22:31 +0100

    Add -- task bitfinex select

---
+++ backend/src/ws/services/bitfinex/bitfinex-task-select.php
@@ -0,0 +1,25 @@
+<?php
+
+include (dirname(__FILE__) . "/bitfinex-ticker-class.php");
+
+$symbolsEndpoint = "https://api.bitfinex.com/v1/symbols";
+$baseEndpoint = "https://api.bitfinex.com/v1/pubticker/";
+
+$conn = new Connection( $servername, $dbname, $user, $password );
+$activeRecord = new ActiveRecord($conn);
+$bitfinex = new BitfinexTicker($activeRecord);
+
+
+//$end = "https://api.bitfinex.com/v1/pubticker/omgusd";
+
+//print_r($bitfinex->getTicker($end));
+
+
+$a = $bitfinex->getAllTickersApi($symbolsEndpoint, $baseEndpoint);
+print_r($a);
+//$ticker = $bitfinex->getAllBTCByVolumeTicker();
+//$ticker = json_encode($ticker);
+
+//echo $ticker;
+
+?>
