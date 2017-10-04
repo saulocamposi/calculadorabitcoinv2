86c18874845f26f444de96b73f9763752a88926d
saulocamposi <saulocamposi@gmail.com>
2017-10-04 11:40:19 +0100

    Add -- create and drop db to automatization

---
+++ backend/src/model/automatization/create_db.php
@@ -0,0 +1,7 @@
+<?php
+
+    $message = "CREATE DATABASE exchanges";
+    $sql = "CREATE DATABASE exchanges";
+
+    include( dirname( __FILE__ , 2) . "/pool_db.php");
+?>
---
+++ backend/src/model/automatization/drop_db.php
@@ -0,0 +1,7 @@
+<?php
+
+    $message = "DROP DATABASE exchanges";
+    $sql = "DROP DATABASE exchanges";
+
+    include( dirname( __FILE__ , 2) . "/pool_db.php");
+?>
