6b1f83c03529265f99f1cd111988ea7f336625f4
saulocamposi <saulocamposi@gmail.com>
2017-10-05 17:23:33 +0100

    Update -- includes

--- backend/src/ws/services/poloniex/poloniex-task-class.php
+++ backend/src/ws/services/poloniex/poloniex-task-class.php
@@ -1,7 +1,8 @@
 <?php
-include (dirname(__FILE__, 5 ) . "/model/core/active-record-class.php");
-include (dirname(__FILE__) . "/poloniex-client-ticker-class.php");
-include (dirname(__FILE__) . "/poloniex-dao-class.php");
+include ( dirname(__FILE__, 5 ) . "/model/core/active-record-class.php" );
+include ( dirname(__FILE__) . "/poloniex-client-ticker-class.php" );
+include ( dirname(__FILE__) . "/poloniex-dao-class.php" );
+include ( dirname(__FILE__) . "/poloniex-sql.php" );
 
 /**
  *
@@ -14,32 +15,44 @@ class PoloniexTask
   private $activeRecord;
   private $poloniexclient;
   private $poloniexdao;
+  private $sql;
+  private $servername;
+  private $dbname;
+  private $user;
+  private $password;
 
-  function __construct(argument)
+
+  function __construct( $bootstrap )
   {
+
+    $this->endpoint = $bootstrap['endpoint'];
+    $this->servername = $bootstrap['servername'];
+    $this->dbname = $bootstrap['dbname'];
+    $this->user = $bootstrap['user'];
+    $this->user = $bootstrap['password'];
+    $this->sql = $bootstrap['sql'];
     $this->initialize();
   }
 
   private function initialize(){
 
-    $this->endpoint = "https://poloniex.com/public?command=returnTicker";
-    $this->conn = new Connection( $servername, $dbname, $user, $password );
-    $this->activeRecord = new ActiveRecord($conn);
-    $this->poloniexclient = new PoloniexClientTicker( $endpoint );
-    $this->poloniexdao = new PoloniexDao( $activeRecord );
+    $this->conn = new Connection( $this->servername, $this->dbname, $this->user, $this->password );
+    $this->activeRecord = new ActiveRecord($this->conn);
+    $this->poloniexclient = new PoloniexClientTicker( $this->endpoint );
+    $this->poloniexdao = new PoloniexDao( $this->activeRecord );
 
   }
 
   public function getJsonAllBtcByVolume()
   {
-    $ticker = $this->poloniexdao->getAllBTCByVolumeTicker();
+    $ticker = $this->poloniexdao->getAllBTCByVolumeTicker( $this->sql );
     $ticker = json_encode($ticker);
     echo $ticker;
   }
 
   public function postTicker()
   {
-    $this->poloniexclient->postTicker();
+    $this->poloniexclient->postTicker( $this->sql );
   }
 
   public static main(){
