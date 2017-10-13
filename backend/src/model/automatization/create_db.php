507ecaf9c2487b6edad76f371d02c95c3ab7c411
saulocamposi <saulocamposi@gmail.com>
2017-10-13 21:05:28 +0100

    Update includes

--- backend/src/model/automatization/create_db.php
+++ backend/src/model/automatization/create_db.php
@@ -3,5 +3,6 @@
     $message = "CREATE DATABASE exchanges";
     $sql = "CREATE DATABASE exchanges";
 
-    include( dirname( __FILE__ , 2) . "/pool_db.php");
+    include( dirname( __FILE__ , 2) . "/core/pool_db.php");
+
 ?>
