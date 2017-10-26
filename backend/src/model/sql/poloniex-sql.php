a69d39d7be0ab666489cd2a7e5a7459a92f53331
saulocamposi <saulocamposi@gmail.com>
2017-10-26 05:30:27 +0100

    Update -- sql sintax

--- backend/src/model/sql/poloniex-sql.php
+++ backend/src/model/sql/poloniex-sql.php
@@ -1,5 +1,11 @@
 <?php
 
+$baseVolume = 0;
+$lowestAsk = 0;
+$highestBid = 0;
+$last = 0;
+$key = 0;
+
 $sql = "INSERT INTO ticker_poloniex (
           id ,
           vol ,
@@ -11,15 +17,20 @@ $sql = "INSERT INTO ticker_poloniex (
         )
         VALUES (
           NULL,
-          $value->baseVolume,
-          $value->lowestAsk,
-          $value->highestBid,
-          $value->last,"
-          . "'" . $key . "'" . ", '" .
+          " . $baseVolume .",
+          " . $lowestAsk . ",
+          " . $highestBid . ",
+          " . $last . ",
+          " . $key .  "," .
           date("Y-m-d H:i:s") . "' );";
 
 
+
+//$sql = "INSERT INTO "
+
 $sql[0] = "SELECT max(id) FROM ticker_poloniex";
+
+$max['max(id)'] = 0;
 
 $sql[1] = "SELECT id,
                   vol,
@@ -33,10 +44,12 @@ $sql[1] = "SELECT id,
           WHERE
           id = " . $max['max(id)'];
 
+
 $query = "SELECT * FROM ticker_poloniex WHERE created_at IN (SELECT max(created_at) FROM ticker_poloniex)";
 
 $query = "SELECT * FROM ticker_poloniex
           WHERE created_at IN (SELECT max(created_at) FROM ticker_poloniex)
           AND pair LIKE 'BTC_%'
           ORDER BY vol DESC;";
+
 ?>
