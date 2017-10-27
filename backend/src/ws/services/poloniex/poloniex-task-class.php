6837595f54ec2e9d69bce2e3ef168c72c06a1aa5
saulocamposi <saulocamposi@gmail.com>
2017-10-27 05:21:04 +0100

    Fix -- main method

--- backend/src/ws/services/poloniex/poloniex-task-class.php
+++ backend/src/ws/services/poloniex/poloniex-task-class.php
@@ -1,12 +1,15 @@
 <?php
-include ( dirname(__FILE__, 5 ) . "/model/core/active-record-class.php" );
-include ( dirname(__FILE__) . "/poloniex-client-ticker-class.php" );
-include ( dirname(__FILE__) . "/poloniex-dao-class.php" );
-include ( dirname(__FILE__) . "/poloniex-sql.php" );
 
+$dirname_model_level = dirname(__FILE__, 5 );
+
+include ( dirname(__FILE__, 5 ) . "/src/model/core/active-record-class.php" );
+include ( dirname(__FILE__, 5 ) . "/src/model/dao/poloniex-dao-class.php" );
+include ( dirname(__FILE__, 5 ) . "/src/model/sql/poloniex-sql.php" );
+include ( dirname(__FILE__) . "/poloniex-client-ticker-class.php" );
 /**
  *
  */
+
 class PoloniexTask
 {
 
@@ -21,7 +24,6 @@ class PoloniexTask
   private $user;
   private $password;
 
-
   function __construct( $bootstrap )
   {
 
@@ -29,15 +31,16 @@ class PoloniexTask
     $this->servername = $bootstrap['servername'];
     $this->dbname = $bootstrap['dbname'];
     $this->user = $bootstrap['user'];
-    $this->user = $bootstrap['password'];
+    $this->password = $bootstrap['password'];
     $this->sql = $bootstrap['sql'];
     $this->initialize();
+
   }
 
   private function initialize(){
 
     $this->conn = new Connection( $this->servername, $this->dbname, $this->user, $this->password );
-    $this->activeRecord = new ActiveRecord($this->conn);
+    $this->activeRecord = new ActiveRecord( $this->conn );
     $this->poloniexclient = new PoloniexClientTicker( $this->endpoint );
     $this->poloniexdao = new PoloniexDao( $this->activeRecord );
 
@@ -45,23 +48,37 @@ class PoloniexTask
 
   public function getJsonAllBtcByVolume()
   {
-    $ticker = $this->poloniexdao->getAllBTCByVolumeTicker( $this->sql );
+    $ticker = $this->poloniexdao->getAllBTCByVolumeTicker( $this->sql['allByVolume'] );
     $ticker = json_encode($ticker);
     echo $ticker;
   }
 
   public function postTicker()
   {
-    $this->poloniexclient->postTicker( $this->sql );
+    $this->poloniexclient->postTicker( $this->sql['postTicker'] );
   }
 
-  public static main(){
-    $task = new PoloniexTask();
-    $param = $_GET['ticker'];
+  public static function  main( $bootstrap )
+  {
+    $task = new PoloniexTask( $bootstrap );
+
+/*
+    if (defined('STDIN')) {
+      $param = $argv[1] != null ? $argv[1] : null ;
+    } else {
+      $param = $_GET['ticker'];
+    }
+
     $param = $param != null ? $task->getJsonAllBtcByVolume() : $task->postTicker();
+    */
+
+    $task->getJsonAllBtcByVolume();
+
   }
 }
 
-PoloniexTask::main();
+$bootstrap['endpoint'] = "https://poloniex.com/public?command=returnTicker";
+
+PoloniexTask::main( $bootstrap );
 
 ?>
