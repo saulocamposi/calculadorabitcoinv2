bfcaaefa5caf3acef7a9abce9507b8c4a902e965
saulocamposi <saulocamposi@gmail.com>
2017-11-15 04:45:32 +0000

    Update -- identation

--- app/js/controllers/foxbitcontroller.js
+++ app/js/controllers/foxbitcontroller.js
@@ -1,14 +1,14 @@
 var app = require('../manager-module.js');
 
-app.controller('foxbitcontroller', ['$scope','$http', function($scope, $http) {
+app.controller('foxbitcontroller', ['$scope','$http', function( $scope, $http ) {
 
     var root_path = "backend/src/ws/services/foxbit/";
 
-    $http.get(root_path + "orders_foxbit_services.php").then(function(response) {
+    $http.get(root_path + "orders_foxbit_services.php").then(function( response ) {
       $scope.foxbitorders = response.data;
     });
 
-    $http.get(root_path + "trades_foxbit_services.php").then(function(response) {
+    $http.get(root_path + "trades_foxbit_services.php").then(function( response ) {
       $scope.foxbittrades = response.data;
     });
 
