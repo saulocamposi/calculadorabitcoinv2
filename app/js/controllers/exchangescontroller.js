2cea7215f7c3a1efd87e92cbaa1e2674fb7c103e
saulocamposi <saulocamposi@gmail.com>
2017-11-21 05:28:38 +0000

    Update exchanges controller

--- app/js/controllers/exchangescontroller.js
+++ app/js/controllers/exchangescontroller.js
@@ -8,31 +8,33 @@ app.controller('exchangescontroller', ['$scope','$http', 'exchangesChartService'
 
     $http.get( root_path + "services.php?brand=fox").then(function(response) {
       var datafoxbit = response.data;
-      dataChart.push({"c" : [{"v" : "FoxBit", "f": null},{"v" : datafoxbit.last, "f": null}]});
+      dataChart.push({"c" : [{"v" : "FOXBIT", "f": null},{"v" : datafoxbit.last, "f": null}]});
       $scope.foxbit = datafoxbit;
     });
 
     $http.get( root_path + "services.php?brand=mbtc").then(function(response) {
       var datambtc = response.data;
-      dataChart.push({"c" : [{"v" : "MTC", "f": null},{"v" : datambtc.last, "f": null}]});
+      dataChart.push({"c" : [{"v" : "MercadoBitcoin", "f": null},{"v" : datambtc.last, "f": null}]});
       $scope.mbtc = datambtc;
     });
 
     $http.get( root_path + "services.php?brand=btctoyou" ).then( function(response) {
       var databtctoyou = response.data;
-      dataChart.push({"c" : [{"v" : "btctoyou", "f": null},{"v" : databtctoyou.last, "f": null}]});
+      dataChart.push({"c" : [{"v" : "Bitcointoyou", "f": null},{"v" : databtctoyou.last, "f": null}]});
       $scope.btctoyou = databtctoyou;
     });
 
+/*
     $http.get( root_path + "services.php?brand=flowbtc").then(function(response) {
       var dataflowbtc = response.data;
       dataChart.push({"c" : [{"v" : "flowbtc", "f": null},{"v" : dataflowbtc.last, "f": null}]});
       $scope.flowbtc = dataflowbtc;
     });
+*/
 
     $http.get( root_path + "services.php?brand=negociecoins").then(function(response) {
       var datanegociecoins = response.data;
-      dataChart.push({"c" : [{"v" : "negociecoins", "f": null},{"v" : datanegociecoins.last, "f": null}]});
+      dataChart.push({"c" : [{"v" : "Negociecoins", "f": null},{"v" : datanegociecoins.last, "f": null}]});
       $scope.negociecoins = datanegociecoins;
     });
 
