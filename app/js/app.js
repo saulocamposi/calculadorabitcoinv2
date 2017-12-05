0178f7a9215720c355d0e21e455c8fcdd7771ae7
saulocamposi <saulocamposi@gmail.com>
2017-12-05 11:51:12 +0000

    Update -- registry bitvalor controller and service

--- app/js/app.js
+++ app/js/app.js
@@ -13,7 +13,6 @@ require("./translate-provider.js");
 
 require("./controllers/calculatorcontroller.js");
 require("./controllers/exchangescontroller.js");
-require("./controllers/bitvalorcontroller.js");
 require("./controllers/foxbitcontroller.js");
 require("./controllers/blinktradecontroller.js");
 require("./controllers/poloniexcontroller.js");
@@ -21,12 +20,15 @@ require("./controllers/poloniexcontrollerv2.js");
 require("./controllers/coinbrcontroller.js");
 require("./controllers/walletfoxbitcontroller.js");
 require("./controllers/bitinkacontroller.js");
+require("./controllers/bitvalorcontroller.js");
 
 
 require("./services/forex.js");
 require("./services/exchangesChartService.js");
 require("./services/blinktradeChartService.js");
 require("./services/bitinkaChartService.js");
+require("./services/bitvalorChartService.js");
+
 require("./services/polionexChartService.js");
 require("./services/foxbitService.js");
 
