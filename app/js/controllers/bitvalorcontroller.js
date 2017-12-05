43ff823a9d8c32444dcda26d00c3ba5f3ed89acf
saulocamposi <saulocamposi@gmail.com>
2017-12-05 11:51:50 +0000

    Update bitvalor chart

--- app/js/controllers/bitvalorcontroller.js
+++ app/js/controllers/bitvalorcontroller.js
@@ -1,11 +1,27 @@
 var app = require('../manager-module.js');
 
-app.controller('bitvalorcontroller', ['$scope','$http', function($scope, $http) {
+app.controller('bitvalorcontroller', ['$scope','$http', 'bitvalorChartService', function($scope, $http, bitvalorChartService) {
 
+    var dataChart = [];
     var root_path = "backend/src/ws/services/bitvalor/";
 
     $http.get(root_path + "bitvalor_services.php").then(function(response) {
-      $scope.bitvalor = response.data;
+      var databitvalor = response.data;
+      $scope.bitvalor = databitvalor;
+
+      console.log(databitvalor.ARN);
+
+      dataChart.push({"c" : [{"v" : "ArenaBitcoin", "f": null},{"v" : databitvalor.ARN.last, "f": null}]});
+      dataChart.push({"c" : [{"v" : "Bitcointoyou", "f": null},{"v" : databitvalor.B2U.last, "f": null}]});
+      dataChart.push({"c" : [{"v" : "BaseBit", "f": null},{"v" : databitvalor.BTD.last, "f": null}]});
+      dataChart.push({"c" : [{"v" : "FOXBIT", "f": null},{"v" : databitvalor.FOX.last, "f": null}]});
+      dataChart.push({"c" : [{"v" : "Flowbtc", "f": null},{"v" : databitvalor.FLW.last, "f": null}]});
+      dataChart.push({"c" : [{"v" : "MercadoBitcoin", "f": null},{"v" : databitvalor.MBT.last, "f": null}]});
+      dataChart.push({"c" : [{"v" : "Negociecoins", "f": null},{"v" : databitvalor.NEG.last, "f": null}]});
+      dataChart.push({"c" : [{"v" : "Localbitcoins", "f": null},{"v" : databitvalor.LOC.last, "f": null}]});
     });
+
+    bitvalorChartService.data.rows = dataChart;
+    $scope.chart = bitvalorChartService;
 
   }]);
