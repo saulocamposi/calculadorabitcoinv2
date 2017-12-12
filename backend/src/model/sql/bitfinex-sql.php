3a27c2ed177f59c6d0bf2492b218b97a80c1c2fd
saulocamposi <saulocamposi@gmail.com>
2017-12-12 18:39:53 +0000

    Add bitfinex sql

---
+++ backend/src/model/sql/bitfinex-sql.php
@@ -0,0 +1,55 @@
+<?php
+
+$baseVolume = 0;
+$lowestAsk = 0;
+$highestBid = 0;
+$last = 0;
+$key = 0;
+
+$postTicker = "INSERT INTO ticker_bitfinex (
+          id ,
+          vol ,
+          low ,
+          high ,
+          last ,
+          pair ,
+          created_at
+        )
+        VALUES (
+          NULL,
+          " . $baseVolume .",
+          " . $lowestAsk . ",
+          " . $highestBid . ",
+          " . $last . ",
+          " . $key .  "," .
+          date("Y-m-d H:i:s") . "' );";
+
+$lastId = "SELECT max(id) FROM ticker_bitfinex";
+
+$max['max(id)'] = $lastId;
+
+$lastTicker = "SELECT id,
+                  vol,
+                  low,
+                  high,
+                  last,
+                  pair,
+                  created_at
+          FROM
+          ticker_bitfinex
+          WHERE
+          id = " . $max['max(id)'];
+
+$all = "SELECT * FROM ticker_bitfinex
+          WHERE created_at IN (SELECT max(created_at) FROM ticker_poloniex)";
+
+$allByVolume = "SELECT * FROM ticker_bitfinex
+          WHERE created_at IN (SELECT max(created_at) FROM ticker_poloniex)
+          AND pair LIKE 'BTC_%'
+          ORDER BY vol DESC;";
+
+$bootstrap['sql']['allByVolume'] = $allByVolume;
+$bootstrap['sql']['postTicker'] = $postTicker;
+
+
+?>
