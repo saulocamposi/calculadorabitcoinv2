1784c6343fd97611735b0c3246f83df97c8db6cb
saulocamposi <saulocamposi@gmail.com>
2017-12-12 19:57:13 +0000

    Add -- mercadobitcoin

---
+++ backend/src/ws/services/mercadobitcoin/exchanges_mbtc.php
@@ -0,0 +1,9 @@
+<?php
+
+     $response =  json_decode(file_get_contents("https://www.mercadobitcoin.net/api/ticker"));
+
+     $ticker = $response->ticker;
+
+     include( dirname( __FILE__ , 4 ) . "/model/seed/insert_ticker_mbtc.php");
+
+?>
