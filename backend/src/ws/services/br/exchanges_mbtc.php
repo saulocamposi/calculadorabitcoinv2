5f9f29ca5146d7b38e2233b577770da881d05484
saulocamposi <saulocamposi@gmail.com>
2017-12-12 18:45:12 +0000

    Delete -- package br

--- backend/src/ws/services/br/exchanges_mbtc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-     $response =  json_decode(file_get_contents("https://www.mercadobitcoin.net/api/ticker"));
-
-     $ticker = $response->ticker;
-
-     include( dirname( __FILE__ , 4 ) . "/model/seed/insert_ticker_mbtc.php");
-
-?>
