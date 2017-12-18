3453ecca124f5520073c064a5bd7c6492443e2ba
saulocamposi <saulocamposi@gmail.com>
2017-12-18 03:24:25 +0000

    Fix -- ticker url

--- backend/src/ws/services/kraken/kraken-ticker.php
+++ backend/src/ws/services/kraken/kraken-ticker.php
@@ -2,7 +2,7 @@
 include ( dirname(__FILE__, 5 ) . "/src/model/sql/kraken-sql.php" );
 include ( dirname(__FILE__, 5 ) . "/src/ws/core/task-class.php" );
 
-$bootstrap['endpoint'] = "https://poloniex.com/public?command=returnTicker";
+$bootstrap['endpoint'] = "https://api.kraken.com/0/public/Ticker?pair=BCHEUR";
 
 Task::main( $bootstrap, $argv );
 
