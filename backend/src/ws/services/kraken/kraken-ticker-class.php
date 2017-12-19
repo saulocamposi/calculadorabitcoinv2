25b36f0108457608896a509b145df53d86806d29
saulocamposi <saulocamposi@gmail.com>
2017-12-19 19:36:49 +0000

    Update wrapper ticker

--- backend/src/ws/services/kraken/kraken-ticker-class.php
+++ backend/src/ws/services/kraken/kraken-ticker-class.php
@@ -19,8 +19,18 @@
 
     public function wrapper()
     {
-      $ticker = json_decode(file_get_contents( "https://api.kraken.com/0/public/Ticker?pair=BCHEUR"));
-      print_r($ticker->result->BCHEUR);
+      $ticker = json_decode(file_get_contents( "https://api.kraken.com/0/public/Ticker?pair=XBTUSD"));
+      print_r($ticker->result->XXBTZUSD);
+
+      $result = $ticker->result->XXBTZUSD;
+
+       $this->ask = $result->a[0];
+       $this->bid = $result->b[0];
+       $this->last_trade = $result->c[0];
+       $this->volume_array = $result->v[0];
+       $this->volume_weighted = $result->p[0];
+       $this->number = $result->t[0];
+
     }
 
 
