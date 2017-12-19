3af510cbbb7de2856359b70cad49a3fb6d0b6fc7
saulocamposi <saulocamposi@gmail.com>
2017-12-19 01:38:02 +0000

    Add kraken ticker class

---
+++ backend/src/ws/services/kraken/kraken-ticker-class.php
@@ -0,0 +1,16 @@
+<?php
+  class KrakenTicker{
+
+    private $ask;
+    private $bid;
+    private $last_trade;
+    private $volume_array;
+    private $volume_weighted;
+    private $number;
+    private $low;
+    private $high;
+    private $today;
+
+    
+  }
+ ?>
