9dcd151af2d503b7064892f8b81424743c69ce1e
saulocamposi <saulocamposi@gmail.com>
2017-12-22 21:34:20 +0000

    Update -- kraken ticker

--- backend/src/ws/services/kraken/kraken-ticker.php
+++ backend/src/ws/services/kraken/kraken-ticker.php
@@ -1,8 +1,11 @@
 <?php
 include ( dirname(__FILE__, 5 ) . "/src/model/sql/kraken-sql.php" );
 include ( dirname(__FILE__, 5 ) . "/src/ws/core/task-class.php" );
+include ( dirname(__FILE__) . "/kraken-ticker-class.php" );
+
 
 $bootstrap['endpoint'] = "https://api.kraken.com/0/public/Ticker?pair=BCHEUR";
+$bootstrap['wrapper'] = new KrakenTicker;
 
 Task::main( $bootstrap, $argv );
 
