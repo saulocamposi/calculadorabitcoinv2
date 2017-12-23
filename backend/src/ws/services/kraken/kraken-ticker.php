2379fb7de1cb09f7d69fa0b3536a63097e685e30
saulocamposi <saulocamposi@gmail.com>
2017-12-23 21:27:34 +0000

    Update -- endpoint test

--- backend/src/ws/services/kraken/kraken-ticker.php
+++ backend/src/ws/services/kraken/kraken-ticker.php
@@ -3,8 +3,9 @@ include ( dirname(__FILE__, 5 ) . "/src/model/sql/kraken-sql.php" );
 include ( dirname(__FILE__, 5 ) . "/src/ws/core/task-class.php" );
 include ( dirname(__FILE__) . "/kraken-ticker-class.php" );
 
+//$bootstrap['endpoint'] = "https://api.kraken.com/0/public/Ticker?pair=BCHEUR";
+$bootstrap['endpoint'] = "https://api.kraken.com/0/public/Ticker?pair=XBTUSD";
 
-$bootstrap['endpoint'] = "https://api.kraken.com/0/public/Ticker?pair=BCHEUR";
 $bootstrap['wrapper'] = new KrakenTicker;
 
 Task::main( $bootstrap, $argv );
