94b4af4e98f3745c17c45e4dcdad52398fcdfa2f
saulocamposi <saulocamposi@gmail.com>
2017-12-24 04:39:20 +0000

    Update -- task kraken instance sql

--- backend/src/ws/services/kraken/kraken-ticker.php
+++ backend/src/ws/services/kraken/kraken-ticker.php
@@ -1,12 +1,12 @@
 <?php
-include ( dirname(__FILE__, 5 ) . "/src/model/sql/kraken-sql.php" );
+include ( dirname(__FILE__, 5 ) . "/src/model/sql/kraken-sql-class.php" );
 include ( dirname(__FILE__, 5 ) . "/src/ws/core/task-class.php" );
 include ( dirname(__FILE__) . "/kraken-ticker-class.php" );
 
 //$bootstrap['endpoint'] = "https://api.kraken.com/0/public/Ticker?pair=BCHEUR";
 $bootstrap['endpoint'] = "https://api.kraken.com/0/public/Ticker?pair=XBTUSD";
-
 $bootstrap['wrapper'] = new KrakenTicker;
+$bootstrap['sql'] = new KrakenSql;
 
 Task::main( $bootstrap, $argv );
 
