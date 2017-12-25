781589989cb134240f05d95da0a913bd54c33aac
saulocamposi <saulocamposi@gmail.com>
2017-12-25 16:58:30 +0000

    Update -- SQL

--- backend/src/ws/services/kraken/kraken-ticker-class.php
+++ backend/src/ws/services/kraken/kraken-ticker-class.php
@@ -4,12 +4,13 @@
     private $ask;
     private $bid;
     private $last_trade;
-    private $volume_array;
+    private $volume;
     private $volume_weighted;
     private $number;
     private $low;
     private $high;
-    private $today;
+    private $open_today;
+    private $pair;
 
     function __construct(){
 
@@ -20,17 +21,50 @@
       //$ticker = json_decode(file_get_contents( "https://api.kraken.com/0/public/Ticker?pair=XBTUSD"));
       //print_r($ticker->result->XXBTZUSD);
 
+
+
+
+
+    /*  <pair_name> = pair name
+    a = ask array(<price>, <whole lot volume>, <lot volume>),
+    b = bid array(<price>, <whole lot volume>, <lot volume>),
+    c = last trade closed array(<price>, <lot volume>),
+    v = volume array(<today>, <last 24 hours>),
+    p = volume weighted average price array(<today>, <last 24 hours>),
+    t = number of trades array(<today>, <last 24 hours>),
+    l = low array(<today>, <last 24 hours>),
+    h = high array(<today>, <last 24 hours>),
+    o = today's opening price
+    */
+
+
+
       print_r($ticker);
 
        $result = $ticker->result->XXBTZUSD;
+       $this->pair = "XBTUSD";
        $this->ask = $result->a[0];
        $this->bid = $result->b[0];
        $this->last_trade = $result->c[0];
-       $this->volume_array = $result->v[0];
+       $this->volume = $result->v[0];
        $this->volume_weighted = $result->p[0];
        $this->number = $result->t[0];
+       $this->low = $result->l[0];
+       $this->high = $result->h[0];
+       $this->open_today = $result->o[0];
 
-       return $sql->postTicker($this->volume_array, $this->volume_array, $this->volume_array, $this->volume_array, $this->volume_array);
+       /*$sql = "CREATE TABLE ticker_kraken (
+               id int NOT NULL AUTO_INCREMENT,
+               vol decimal(16,8) ,
+               low decimal(16,8) ,
+               high decimal(16,8) ,
+               last decimal(16,8) ,
+               pair varchar(30) ,
+               created_at DATETIME,
+               PRIMARY KEY (`id`)
+               )";
+       */
+       return $sql->postTicker($this->volume, $this->low, $this->high, $this->last_trade, $this->pair);
 
       }
   }
