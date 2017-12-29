d761786814eed86707722355411d6afb9dc30b83
saulocamposi <saulocamposi@gmail.com>
2017-12-29 03:44:00 +0000

    Add bitfinex wrapper

---
+++ backend/src/ws/services/bitfinex/bitfinex-ticker.php
@@ -0,0 +1,13 @@
+<?php
+include ( dirname(__FILE__, 5 ) . "/src/model/sql/bitfinex-sql-class.php" );
+include ( dirname(__FILE__, 5 ) . "/src/ws/core/task-class.php" );
+include ( dirname(__FILE__) . "/bitfinex-ticker-class.php" );
+
+//$bootstrap['endpoint'] = "https://api.kraken.com/0/public/Ticker?pair=BCHEUR";
+$bootstrap['endpoint'] = "https://api.bitfinex.com/v1/pubticker/btcusd";
+$bootstrap['wrapper'] = new BitfinexTicker;
+$bootstrap['sql'] = new BitfinexSql;
+
+Task::main( $bootstrap, $argv );
+
+?>
