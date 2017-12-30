fa4051ea8ea3d2b18946a269fe4008895c2fa566
saulocamposi <saulocamposi@gmail.com>
2017-12-30 21:06:35 +0000

    Update -- endpoint symbols

--- backend/src/ws/services/bitfinex/bitfinex-ticker.php
+++ backend/src/ws/services/bitfinex/bitfinex-ticker.php
@@ -4,6 +4,7 @@ include ( dirname(__FILE__, 5 ) . "/src/ws/core/task-class.php" );
 include ( dirname(__FILE__) . "/bitfinex-ticker-class.php" );
 
 //$bootstrap['endpoint'] = "https://api.kraken.com/0/public/Ticker?pair=BCHEUR";
+$bootstrap['endpoint_symbols'] = "https://api.bitfinex.com/v1/pubticker/btcusd";
 $bootstrap['endpoint'] = "https://api.bitfinex.com/v1/pubticker/btcusd";
 $bootstrap['wrapper'] = new BitfinexTicker;
 $bootstrap['sql'] = new BitfinexSql;
