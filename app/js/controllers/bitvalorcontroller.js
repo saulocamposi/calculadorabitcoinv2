7c4a4b8c07167f54cca28dacd0aa74e20ce8a5d0
saulocamposi <saulocamposi@gmail.com>
2018-02-13 15:55:43 +0000

    Fix -- remove temporarly arena bitcoin

--- app/js/controllers/bitvalorcontroller.js
+++ app/js/controllers/bitvalorcontroller.js
@@ -9,7 +9,7 @@ app.controller('bitvalorcontroller', ['$scope','$http', 'bitvalorChartService', 
       var databitvalor = response.data;
       $scope.bitvalor = databitvalor;
 
-      dataChart.push({"c" : [{"v" : "ArenaBitcoin", "f": null},{"v" : databitvalor.ARN.last, "f": null}]});
+      //dataChart.push({"c" : [{"v" : "ArenaBitcoin", "f": null},{"v" : databitvalor.ARN.last, "f": null}]});
       dataChart.push({"c" : [{"v" : "Bitcointoyou", "f": null},{"v" : databitvalor.B2U.last, "f": null}]});
       dataChart.push({"c" : [{"v" : "BaseBit", "f": null},{"v" : databitvalor.BTD.last, "f": null}]});
       dataChart.push({"c" : [{"v" : "FOXBIT", "f": null},{"v" : databitvalor.FOX.last, "f": null}]});
