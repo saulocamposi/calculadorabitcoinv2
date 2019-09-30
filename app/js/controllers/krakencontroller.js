3fa344c6c333c56b7266a620fb2abb830fadf4f2
saulocamposi <saulocamposi@gmail.com>
2019-09-30 19:53:14 +0100

    Update kraken js controller

---
+++ app/js/controllers/krakencontroller.js
@@ -0,0 +1,15 @@
+var app = require('../manager-module.js');
+
+app.controller('krakencontroller', ['$scope','$http', function($scope, $http) {
+
+    var dataChart = [];
+
+    var root_path = "backend/src/ws/services/kraken/";
+
+
+    $http.get(root_path + "poloniex-task-select.php").then(function(response) {
+      var ticker = response.data;
+      $scope.tickerpoloniex = ticker;
+    });
+
+  }]);
